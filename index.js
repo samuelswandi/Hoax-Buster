67c757cb1f0ae9b89fb779a167b7966ee2fdab5f
Samuel Swandi <samuel.swandi@gmail.com>
2021-08-10 20:53:58 +0700

    async

--- index.js
+++ index.js
@@ -1,98 +1,93 @@
-const https = require("https")
-const express = require("express")
-const app = express()
-const PORT = process.env.PORT || 3000
-const TOKEN = process.env.LINE_ACCESS_TOKEN
+const https = require("https");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const TOKEN = process.env.LINE_ACCESS_TOKEN;
 
-app.use(express.json())
-app.use(express.urlencoded({
-  extended: true
-}))
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 app.get("/", (req, res) => {
-  res.sendStatus(200)
-})
+  res.sendStatus(200);
+});
+
+const getData = async (Google_URL) => {
+  const response = await fetch(Google_URL);
+  const data = await response.json();
+  const rating = data.claims[0].claimReview[0].textualRating;
+  const url = data.claims[0].claimsReview[0].url;
+  return { rating, url };
+};
 
 app.post("/webhook", function (req, res) {
-  res.send("HTTP POST request sent to the webhook URL!")
+  res.send("HTTP POST request sent to the webhook URL!");
   // If the user sends a message to your bot, send a reply message
   if (req.body.events[0].type === "message") {
-    var rating = ""
-    var url = ""
-    var textInput = req.body.events[0].message.text
+    var textInput = req.body.events[0].message.text;
 
     var sendToGoogle = {
       query: textInput,
-      key: "AIzaSyAEiE1lYgFP5ZZ_vDba0moCJ_5v8hrvSe8"
-    }
+      key: "AIzaSyAEiE1lYgFP5ZZ_vDba0moCJ_5v8hrvSe8",
+    };
 
     var esc = encodeURIComponent;
     var query = Object.keys(sendToGoogle)
-      .map(k => esc(k) + '=' + esc(sendToGoogle[k]))
-      .join('&');
+      .map((k) => esc(k) + "=" + esc(sendToGoogle[k]))
+      .join("&");
 
-    const URL = "https://factchecktools.googleapis.com/v1alpha1/claims:search?"
-    var Google_URL = URL + query
+    const URL = "https://factchecktools.googleapis.com/v1alpha1/claims:search?";
+    var Google_URL = URL + query;
 
-    https.get(Google_URL, (resp) => {
-      var data = '';
-      resp.on('data', (chunk) => {
-        data += chunk;
-      });
-    
-      resp.on('end', () => {
-          rating = JSON.parse(data).claims[0].claimReview[0].textualRating
-          url = JSON.parse(data).claims[0].claimReview[0].url
-      });
-    
-    }).on("error", (err) => {
-      console.log("Error: " + err.message);
-    });
+    let { rating, url } = getData(Google_URL);
 
     // Message data, must be stringified
     const dataString = JSON.stringify({
       replyToken: req.body.events[0].replyToken,
       messages: [
         {
-          "type": "text",
-          "text": rating + url
-        }
-      ]
-    })
+          type: "text",
+          text: rating + url,
+        },
+      ],
+    });
 
     // Request header
     const headers = {
       "Content-Type": "application/json",
-      "Authorization": "Bearer " + TOKEN
-    }
+      Authorization: "Bearer " + TOKEN,
+    };
 
     // Options to pass into the request
     const webhookOptions = {
-      "hostname": "api.line.me",
-      "path": "/v2/bot/message/reply",
-      "method": "POST",
-      "headers": headers,
-      "body": dataString
-    }
+      hostname: "api.line.me",
+      path: "/v2/bot/message/reply",
+      method: "POST",
+      headers: headers,
+      body: dataString,
+    };
 
     // Define request
     const request = https.request(webhookOptions, (res) => {
       res.on("data", (d) => {
-        process.stdout.write(d)
-      })
-    })
+        process.stdout.write(d);
+      });
+    });
 
     // Handle error
     request.on("error", (err) => {
-      console.error(err)
-    })
+      console.error(err);
+    });
 
     // Send data
-    request.write(dataString)
-    request.end()
+    request.write(dataString);
+    request.end();
   }
-})
+});
 
 app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`)
-})
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
