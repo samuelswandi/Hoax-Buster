fb14e0f11de28aeb17c804bb206f38c58b94db40
Samuel Swandi <samuel.swandi@gmail.com>
2021-08-10 21:23:49 +0700

    configure data:

--- index.js
+++ index.js
@@ -21,10 +21,10 @@ const getData = async (Google_URL) => {
   try {
     const response = await fetch(Google_URL);
     const data = await response.json();
+    console.log(data)
     console.log(typeof(data))
     const rating = data.claims[0].claimReview[0].textualRating;
     const url = data.claims[0].claimsReview[0].url;
-    console.log('MASUK GET DATA')
     return { rating, url };
   } catch (err) {
     console.log(err)
